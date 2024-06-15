1520d87b3d467870b1c54112b8bea6f1490540fe
suleiman ibrahim inuwa <isuleimanibrahim@gmail.com>
2024-06-15 02:31:36 -0700

    first commit

---
+++ src/main/java/com/assessment/eventbookingsystem/EventBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.eventbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/eventbookingsystem/EventBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.eventbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
